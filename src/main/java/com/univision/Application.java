8c005356f010abf50964ce67aad1e28709dce680
jacobben85 <jacobben85@gmail.com>
2015-10-02 12:53:34 -0400

    Successful run

--- src/main/java/com/univision/Application.java
+++ src/main/java/com/univision/Application.java
@@ -1,45 +1,27 @@
 package com.univision;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.univision.feedsyn.FeedProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 @SpringBootApplication
-@EnableBatchProcessing
 public class Application {
-
-    @Autowired
-    private JobBuilderFactory jobs;
-
-    @Autowired
-    private StepBuilderFactory steps;
-
-    @Bean
-    protected Tasklet tasklet() {
-        return (contribution, context) -> RepeatStatus.FINISHED;
-    }
-
-    @Bean
-    public Job job() throws Exception {
-        return this.jobs.get("job").start(step1()).build();
-    }
-
-    @Bean
-    protected Step step1() throws Exception {
-        return this.steps.get("step1").tasklet(tasklet()).build();
-    }
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        System.exit(SpringApplication.exit(ctx));
+
+        FeedProcessor fp = new FeedProcessor();
+        try {
+            String response = fp.processFeed("stats", "832695");
+            System.out.println(response);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
